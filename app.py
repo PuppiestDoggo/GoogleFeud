5d403e3a4ee42f40fcdf3b04582c88574390cb11
blackmine57 <blackmine57@protonmail.com>
2023-11-16 18:04:01 +0100

    v0.0.0.0.0.0.1

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
+import requests
+endpoint="http://127.0.0.1:8000"
 
 app = Flask(__name__)
 
 
-@app.route('/')
+headers = {
+    "Content-Type": "application/json"
+}
+@app.route('/play')
 def hello_world():  # put application's code here
-    return 'Hello World!'
-
+    query = requests.get(f"{endpoint}/getquery/", headers=headers).json()
+    return render_template('play.html',query=query)
 
 if __name__ == '__main__':
     app.run()
