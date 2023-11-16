4f60827fe246c7c3f9f0ddf917a9abe207bc2aff
blackmine57 <blackmine57@protonmail.com>
2023-11-16 21:15:35 +0100

    Worst code ever

--- app.py
+++ app.py
@@ -16,7 +16,20 @@ headers = {
 @app.route('/play')
 def hello_world():  # put application's code here
     query = requests.get(f"{endpoint}/getquery/", headers=headers).content.decode("utf-8").replace('"','')
-    answers = ast.literal_eval(requests.get(f"{endpoint}/query/{query}", headers=headers).content.decode("utf-8"))["answers"]
+    oldAnswers = ast.literal_eval(requests.get(f"{endpoint}/query/{query}", headers=headers).content.decode("utf-8"))["answers"]
+
+    answers = []
+    y = 0
+
+    for i in oldAnswers:
+        if y < 10:
+            modified_i = i.replace(query.lower(), '')
+            answers.append(modified_i)
+            y += 1
+            print(query.lower())
+            print(answers)
+            print(oldAnswers)
+
     return render_template('play.html', query=query, answers=answers)
 
 
